061c895fa6dc763fcb632d6d77121e3573bf055d
camilocar12 <camiloa.carranza@gmail.com>
2026-03-24 21:30:56 -0500

    Update og-page.js

--- netlify/og-page.js
+++ netlify/og-page.js
@@ -1,9 +1,10 @@
-// Return a local image path if no original is provided
+// Forzamos a que siempre devuelva la ruta de home.png
 const imageURL = function(data) {
   if(data.original_og) {
     return `${data.original_og}`;
   } else {
-    return `${data.domain}/image${data.path}.png`;
+    // No importa el data.path, siempre retornamos /image/home.png
+    return `${data.domain}/image/home.png`;
   }
 };
 
@@ -17,16 +18,16 @@ export default (data) => {
    <title>Find that at...</title>
    <meta name="twitter:card" content="summary_large_image"/>
    <meta name="twitter:site" content="${data.site}"/>
-   <meta name="twitter:title" content="${data.title}"/>
-   <meta name="twitter:description" content="${data.description}"/>
+   <meta name="twitter:title" content="¡Estás invitado a nuestra boda! 💍"/>
+   <meta name="twitter:description" content="Acompáñanos el 12 de diciembre a celebrar nuestro gran día. Haz clic para ver los detalles."/>
    <meta name="twitter:creator" content="${data.site}"/>
    <meta name="twitter:image" content="${imageURL(data)}"/>
    <meta name="og:image" content="${imageURL(data)}"/>
    <meta name="og:image:secure_url" content="$${imageURL(data)}"/>
    <meta name="image" property="og:image" content="${imageURL(data)}"/>
-   <meta property="og:site_name" content="${data.site}"/>
-   <meta property="og:title" content="${data.title}"/>
-   <meta property="og:description" content="${data.description}"/>
+   <meta property="og:site_name" content="Boda Camilo & Alejandra"/>
+   <meta property="og:title" content="¡Estás invitado a nuestra boda! 💍"/>
+   <meta property="og:description" content="Acompáñanos el 12 de diciembre a celebrar nuestro gran día. Haz clic para ver los detalles."/>
  </head>
  <body>
   A little OG image help from <a href="https://hawksworx.com">Phil Hawksworth</a>
